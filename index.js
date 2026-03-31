ef8c6fc478a4ef3e7fae8f870f8ea49f2475158b
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2026-03-31 11:07:09 +0000

    Serve Email

--- index.js
+++ index.js
@@ -2,23 +2,51 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-// Basic Configuration
+var { registerTestRoutes } = require('w4j-test-utils');
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
+/*
+Your code Goes Here
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+registerTestRoutes(app);
+// Listen on port set in environment variable or default to 3000
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
